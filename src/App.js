4bdc8b175a38df2056b37682dcb40203ebec75e8
moontracer <themoontracer@gmail.com>
2019-11-15 15:06:56 -0500

    question page progress, bit antsy on the design though.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import MobileNav from './Views/Mobile/MobileNav.js';
 import MobileCharHeader from './Views/Mobile/MobileCharHeader.js';
 import MobileCharInfo from './Views/Mobile/MobileCharInfo.js';
+import MobileQuestions from './Views/Mobile/MobileQuestions.js';
 
 function App() {
   return (
     <div className="App">
       <MobileNav />
-      <MobileCharHeader />
-      <MobileCharInfo />
+      {/* The character overview page content here, working on Q&A page atm */}
+      {/* <MobileCharHeader />
+      <MobileCharInfo /> */}
+      <MobileQuestions />
     </div>
   );
 }
---
+++ src/Views/Mobile/MobileQuestions.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+//State to track with this class: Current page that the user is on
+
+class MobileQuestions extends React.Component {
+    render(){
+        return (
+            <div>
+                <div id="QAButtonContainer">
+                    <button className="QAButton">Questions</button>
+                    <button className="QAButton">Ask</button>
+                </div>
+                {/* Question Search Container */}
+                <div id="searchContainer">
+                <input id="questionSearch" type="search" placeholder="Search..." />
+                </div>
+                <section id="questionContainer">
+                    {/* Question Content here */}
+                    <h3>Moon • <span id="questionTimestamp">12h</span></h3>
+                    <h2>I'm having trouble landing the double DP route. Any tips?</h2>
+                </section>
+                <div id="pageMoveContainer">
+                    <button className="pageMoveButtons">&lt; Previous</button>
+                    <button className="pageMoveButtons">Next &gt;</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MobileQuestions;
