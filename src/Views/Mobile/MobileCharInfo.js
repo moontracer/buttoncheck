8ee9e16c0c7ec46b0186813422963ec5d1c2e5b3
moontracer <themoontracer@gmail.com>
2019-11-15 12:22:36 -0500

    good overview page, working on Q&A template

--- src/Views/Mobile/MobileCharInfo.js
+++ src/Views/Mobile/MobileCharInfo.js
@@ -8,9 +8,14 @@ class MobileCharInfo extends React.Component {
                 <img id="charImage" src={gbvsKat} alt="characterImage" />
                 {/* Character Information */}
                 <div>
-                    <p>PLAYSTYLE: </p>
+                    {/* <p>PLAYSTYLE: </p>
                     <p>STRENGTHS: </p>
-                    <p>WEAKNESSES: </p>
+                    <p>WEAKNESSES: </p> */}
+                    <p id="overviewDescription">Katalina is a character that takes advantage of her superior range and damage. 
+                        Most of her normals control space very well and lead into high damage in many situations. 
+                        Although she lacks good traditional mixup, she is heavily rewarded for being as safe as possible and punishing her 
+                        opponents' mistakes. Even though her normals have excellent range, they are relatively slow, which can cause her 
+                        trouble on defense.</p>
                 </div>
             </div>
         )
