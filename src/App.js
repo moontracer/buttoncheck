4d08d1221e5d7fc553942149cc7641377ec50377
moontracer <themoontracer@gmail.com>
2019-11-28 13:20:24 -0500

    great registration template, needs to be cleaned up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './App.css';
-import MobileNav from './Views/Mobile/MobileNav.js';
+// import MobileNav from './Views/Mobile/MobileNav.js';
 // import MobileCharHeader from './Views/Mobile/MobileCharHeader.js';
 // import MobileCharInfo from './Views/Mobile/MobileCharInfo.js';
 // import MobileQuestions from './Views/Mobile/MobileQuestions.js';
-// import MobileRegistration from './Views/Mobile/MobileRegistration.js';
-import MobileCharForum from './Views/Mobile/MobileCharForum';
+import MobileRegistration from './Views/Mobile/MobileRegistration.js';
+// import MobileCharForum from './Views/Mobile/MobileCharForum';
 
 function App() {
   return (
     <div className="App">
-      <MobileNav />
+      {/* <MobileNav /> */}
       {/* The character overview page content here, working on Q&A page atm */}
       {/* <MobileQuestions /> */}
       {/* <MobileCharHeader /> */}
       {/* <MobileCharInfo /> */}
-      {/* <MobileRegistration /> */}
-      <MobileCharForum />
+      <MobileRegistration />
+      {/* Mobile character forum basically finished, working on registration now */}
+      {/* <MobileCharForum /> */}
     </div>
   );
 }
--- src/Views/Mobile/MobileRegistration.js
+++ src/Views/Mobile/MobileRegistration.js
@@ -3,11 +3,28 @@ import React from 'react';
 class MobileRegistration extends React.Component {
     render(){
         return (
+            <div className="infoForm">
+            <h1 id="signupLabel">Create an Account</h1>
             <form method="POST">
-                <input type="text" placeholder="Username" name="name" />
-                <input type="password" placeholder="Password" name="pass" />
-                <input type="Submit" value="Submit" />
+                {/* infoText is Not A Great Name */}
+                {/* Uses html labels for Username, Email, Pass, and Pronouns. 
+                Explain what each option is with a little subtext. */}
+                <label htmlFor="registerName">Username</label>
+                <input className="infoText" type="text" name="name" id="registerName" />
+                <p>Your username may <strong>not</strong> contain spaces.</p>
+                <label htmlFor="registerPass">Password</label>
+                <input className="infoText" type="password" name="pass" id="registerPass" />
+                <p>Use something nice and sweet that you can remember!</p>
+                <label htmlFor="registerEmail">Email</label>
+                <input className="infoText" type="email" name="userEmail" id="registerEmail" />
+                <p>Your email address won't be given to anyone.</p>
+                <label htmlFor="registerPronouns">Pronouns</label>
+                <input className="infoText" type="text" name="pronouns" id="registerPronouns" /> 
+                <p>Please list your pronouns here! <strong>Users that register with joke pronouns will be banned.</strong></p>
+                {/* Submit button needs an onchange handler*/}
+                <input type="Submit" className="infoSubmit" value="Register" />
             </form>
+            </div>
         )
     }
 }
