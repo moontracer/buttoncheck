0987953c3451a922e4234e2bfe38e2347d43da64
moontracer <themoontracer@gmail.com>
2019-11-30 09:12:13 -0500

    working template animation, clean up

--- src/Views/Mobile/MobileNavMenu.js
+++ src/Views/Mobile/MobileNavMenu.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 function MobileNavMenu(props){
     return (
-        <div className="navSideMenu">
         <ul id={props.menuActivated ? "navSideMenuShow" : "navSideMenuHide"}>
             <li>Katalina</li>
             <li>Zeta</li>
@@ -10,7 +9,6 @@ function MobileNavMenu(props){
             <li>Metera</li>
             <li>Ferry</li>
         </ul>
-        </div>
     );
 }
 
