acad76961937963bbd2c9f7de2cf5b6fd942c0ef
moontracer <themoontracer@gmail.com>
2019-11-30 12:09:02 -0500

    using transitions instead of animation

--- src/Views/Mobile/MobileNavMenu.js
+++ src/Views/Mobile/MobileNavMenu.js
@@ -3,11 +3,17 @@ import React from 'react';
 function MobileNavMenu(props){
     return (
         <ul id={props.menuActivated ? "navSideMenuShow" : "navSideMenuHide"}>
+            <li>Gran</li>
             <li>Katalina</li>
-            <li>Zeta</li>
+            <li>Charlotta</li>
+            <li>Lancelot</li>
             <li>Percival</li>
-            <li>Metera</li>
             <li>Ferry</li>
+            <li>Lowain</li>
+            <li>Ladiva</li>
+            <li>Metera</li>
+            <li>Zeta</li>
+            <li>Vaseraga</li>
         </ul>
     );
 }
