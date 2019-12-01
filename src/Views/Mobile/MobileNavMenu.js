210674da37329e63fc78495119bbfcc38efcaa28
moontracer <themoontracer@gmail.com>
2019-11-30 21:41:56 -0500

    did templating work but need to update design conventions

--- src/Views/Mobile/MobileNavMenu.js
+++ src/Views/Mobile/MobileNavMenu.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import subforumPFPTest from '../../images/subforumPFPTest.png';
 
 function MobileNavMenu(props){
     return (
         <ul className={props.menuActivated ? "navSideMenuShow" : "navSideMenuHide"}>
-            <li id="navSideMenuHead" className="navSideMenuItem">Gran</li>
-            <li className="navSideMenuItem">Katalina</li>
+            <li id="navSideMenuHead" className="navSideMenuItem"><img src={subforumPFPTest} alt="gran" className="resizeTest"/>Gran</li>
+            <li className="navSideMenuItem"><img src={subforumPFPTest} alt="katalina" className="resizeTest" />Katalina</li>
             <li className="navSideMenuItem">Charlotta</li>
             <li className="navSideMenuItem">Lancelot</li>
             <li className="navSideMenuItem">Percival</li>
