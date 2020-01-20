3d78c4ea111f9d55e4ff19a59c6695ed37eea827
moontracer <themoontracer@gmail.com>
2020-01-20 16:27:39 -0500

    defined api routes and rough draft of DB queries

--- DBQuery.sql
+++ DBQuery.sql
@@ -1,74 +1,87 @@
 /*
 
-USE SkyboundArts
-
-CREATE TABLE dbo.Videos (
-	VideoID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	VODEventID INT NOT NULL,
-	FOREIGN KEY (VODEventID)
-	REFERENCES dbo.Events(EventID),
-	P1_NameID INT NOT NULL,
-	P2_NameID INT NOT NULL,
-	WinnerPlayer INT NOT NULL,
-	FOREIGN KEY (P1_NameID, P2_NameID, WinnerPlayer)
-	REFERENCES dbo.Players(PlayerID),
-	P1_Character INT NOT NULL,
-	P2_Character INT NOT NULL,
-	WinnerCharacter INT NOT NULL,
-	FOREIGN KEY (P1_Character, P2_Character, WinnerCharacter)
-	REFERENCES dbo.Characters(CharacterID),
-	VideoLink VARCHAR(100) NOT NULL
-);
-
-GO
-
-*/
-
-CREATE TABLE dbo.Videos (
-	VideoID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	Event_Name NVARCHAR(100) NOT NULL,
-	FOREIGN KEY (Event_Name)
-	REFERENCES dbo.Events(Name),
-
-);
-
-GO
-
-/*
-EventID and Name (Unique) for Events
-
-players table - ?!? IDK! Look into this. Drop the players table and have it reference the Videos table.
-	Gonna have two fields - PlayerID and Name. Name is going to be unique.
-
-Characters table - two fields. CharacterID and Name. Make name unique.
-
-A foreign key constraint does not have to be linked only to a primary key constraint in another table; 
-it can also be defined to reference the columns of a UNIQUE constraint in another table.
-
-Things that would be unique - (ex. Emails)
-in this instance, what would be unique is a player's name?!?
-	Revisit this.
-
-So the thing that determines whether or not a player is in the players table is if they have been featured in a video
-Meaning, that I was seeing things backwards. Instead of Videos referencing Players, Players should be referencing videos.
-
-P1Char, P2Char, WinnerChar should all reference the Characters table
-Multiple columns to reference the same foreign key - it'd make most sense to target by characterID
-I think that I'd have to create 3 foreign keys
-
-Another thing that would be unique - Events
-	This means that instead of targeting EventID, you can directly use the event name
-Another thing that would be unique - Video Links
-	This means that Video Links can be unique in the Videos table
-
-So we can have a table of player names right and 
-
-So, from reading this - define FKs as unique and not null
-
-
-
 CREATE TABLE dbo.Characters (
-	CharacterID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	CharacterID INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
+	Character_Name NVARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE dbo.Events (
+	EventID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Event_Name NVARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE dbo.Players (
+	PlayerID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Player_Name NVARCHAR(60) UNIQUE NOT NULL
+);
+
+CREATE TABLE dbo.Videos (
+	VideoID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	EventName NVARCHAR(100) NOT NULL,
+	P1_Character NVARCHAR(50) NOT NULL,
+	P2_Character NVARCHAR(50) NOT NULL,
+	WinnerCharacter NVARCHAR(50) NOT NULL,
+	FOREIGN KEY (P1_Character)
+	REFERENCES dbo.Characters(Character_Name),
+	FOREIGN KEY (P2_Character)
+	REFERENCES dbo.Characters(Character_Name),
+	FOREIGN KEY (WinnerCharacter)
+	REFERENCES dbo.Characters(Character_Name),
+	P1_Player NVARCHAR(60) NOT NULL,
+	P2_Player NVARCHAR(60) NOT NULL,
+	WinnerPlayer NVARCHAR(60) NOT NULL,
+	VideoLink NVARCHAR(120) NOT NULL
+);
+
+GO
+
+INSERT INTO dbo.Characters
+	([Character_Name])
+VALUES
+	(N'Gran'),
+	(N'Katalina'),
+	(N'Charlotta'),
+	(N'Lancelot'),
+	(N'Percival'),
+	(N'Ferry'),
+	(N'Lowain'),
+	(N'Ladiva'),
+	(N'Metera'),
+	(N'Zeta'),
+	(N'Vaseraga')
+GO
+
+INSERT INTO dbo.Videos
+([EventName], [P1_Character], [P2_Character], [WinnerCharacter], [P1_Player], [P2_Player], [WinnerPlayer],
+[VideoLink]
 )
+VALUES
+	(N'Granblue Fantasy Fes 2019', N'Metera', N'Ladiva', N'Ladiva', N'Spooky', N'freakmasta', N'freakmasta', N'https://youtu.be/mUYrqn7y4yY?t=35'),
+	(N'ArcRevo 2019', N'Lancelot', N'Percival', N'Lancelot', N'Kizzie Kay', N'BeautifulDude', N'Kizzie Kay', N'https://youtu.be/z7jnI4RY32k?t=2172')
+GO
+
+These default insertions into the Events and Players table are temporary. I'm just going to be using them to ensure
+that my API calls work.
+
+INSERT INTO dbo.Events
+([Event_Name])
+VALUES
+	(N'Granblue Fantasy Fes 2019'),
+	(N'ArcRevo 2019')
+GO
+
+INSERT INTO dbo.Players
+([Player_Name])
+VALUES
+	(N'Spooky'),
+	(N'freakmasta'),
+	(N'BeautifulDude'),
+	(N'Kizzie Kay')
+GO
+
+The information that's placed into these tables is based on the event and player entries from the videos table.
+
+After every video insertion, a query atttempt will be made to insert an event or player into the table. If the
+player or Event has been entered into the table before, it won't go through.
 
 */
--- README.md
+++ README.md
@@ -69,3 +69,82 @@ The main question that I have right now is this:
 
 How will I know which video the user is reporting when they click that button? It's the thing that's currently tossing me for the biggest loop.
 
+
+--
+Semantic UI Dropdown
+Create database
+    (I should probably handle this tonight or tomorrow morning)
+
+
+ReadME Update (1/20):
+
+Database has been completed. The next task that I must do is hook up the database with my Entity Framework Application!
+
+Next, I'm going to define all the requests that we're going to use for this API and backend.
+
+So! We have the following tables: Characters, Events, Players, and Videos - plus I'm going to need CRUD functionality here (Create, Read, Update, Delete).
+
+What aspects of the character functionality are important? Let's think about that here....
+Create Character
+    Well, I don't need to write an API call for this as this is something I will only handle in the database.
+Get Character
+    This will be used when the user submits a video.
+
+    On second thought, I'm gonna say never mind and say that I stand corrected here actually. The user is going to be selecting either
+    "Any Character" (BIG NOTE WRITE CODE FOR THAT - Not too sure on how I'm going to handle that right now but we'll see as the app develops) OR they'll be selecting a character from 1 of 11.
+
+    So! To loop around and correct myself, this will be used when the user submits a video but not for verification purposes.
+
+    This will also be used when the user attempts to search by character, it will grab all VODs featuring character X.
+
+Update Character
+    Not important and not relevant
+Delete Character
+    Not important and not relevant
+
+Events
+    Create Event
+        Events will be created (if they don't exist) when the user submits a video
+    Get Events
+        When the user attempts to search by event name, it will grab all VODs tagged with event X.
+    Update Events
+        Not important and not relevant FOR NOW but I can always add this functionality later! This is post-MVP most definitely.
+    Delete Events
+        Not important and not relevant
+
+Players
+    Create Player
+        Players will be created (if they don't exist) when the user submits a video
+    Get Player
+        When the user attempts to search by player name, it will grab all VODs featuring player X.
+    Update Player
+        Not impotant and not relevant atm, something I can just do from the backend if an issue arises.
+    Delete Player
+        Not important and not relevant
+
+Videos
+    Create Videos
+        Of course users need the permission to do this!! Big part of the application.
+
+        IMPORTANT TIDBIT ON UPDATING VIDEOS
+        Hmmm....I dunno really. I think that I like the idea of not implementing this into the application right away and see what the reception and usage is like. I'm a bit wary that users will tamper with the website and I'd also have to look into who edited the site and made changes.
+
+        This also calls to mind one other question I have.
+
+        If I use Discourse as my SSO Provider, I wonder if it'd be possible for me to see the user who uploaded something to the website in the backend. Perhaps a UploadedUser field in the database? I wonder if that would be possible. I'll look into it after I get more of this backend stuff handled.
+
+    Get Videos
+        I can't really think of a reason why I would grab a video by its ID.
+    Delete Videos
+        Something that I'll handle from the backend directly with TSQL, not a frontend necessity atm.
+
+So, this is the final list of API calls that I need / things I will use:
+
+Create Videos (w/ UploadedUser post-implementation, perhaps an uploadDate column too so I can just present information like that? Hmm...)
+Create Player (will be done when the user submits a video if the player doesn't already exist in the player database)
+Get Player (user able to search by player name and find all VODs featuring player X - THis actually applies to 
+the videos section, I'd have to do API searches that checks Player1Character then Player2Character for a name
+)
+Create Event (will be done when the user submits a video if the event doesn't already exist in the event database)
+Get Event (will search the Videos table for all VODs tagged with event X)
+GetCharacter (will search the Videos table for all VODs featuring character X)
